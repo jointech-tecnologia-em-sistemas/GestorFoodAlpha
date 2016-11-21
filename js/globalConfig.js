51bb010e722faba48cbc16bf922dd7bc4a6cba04
witerlland blando <witerlland blando>
2016-11-21 14:49:06 -0300

    Mudança no ip da aplicação e inicio da produção da funcao de mudansa de comandas

--- js/globalConfig.js
+++ js/globalConfig.js
@@ -1,9 +1,9 @@
 	// generalIP = 'http://192.168.1.136:8080';
 	// generalIP = 'http://localhost';
 	generalIP = 'http://192.168.1.99';
-	generalUrl 				= generalIP +'/gestorFoodMobile/';
-	generalControllerUrl 	= 'app/mvc/controller';
-	generalModelUrl 		= 'app/mvc/model';
-	generalViewUrl 			= 'app/mvc/view';
-	
+	generalUrl = generalIP + '/gestorFoodMobile/';
+	generalControllerUrl = 'app/mvc/controller';
+	generalModelUrl = 'app/mvc/model';
+	generalViewUrl = 'app/mvc/view';
+
 	global_debug = 'no';
