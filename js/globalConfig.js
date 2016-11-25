371741f1549408ae9388621a3cbdac8b6932922b
witerlland blando <witerlland blando>
2016-11-25 09:14:29 -0300

    Modificações para funcionamento offline do sistema para testes

--- js/globalConfig.js
+++ js/globalConfig.js
@@ -1,16 +1,8 @@
-	generalIP = 'http://192.168.1.99';
-<<<<<<< HEAD
-	generalUrl = generalIP + '/gestorFoodMobile/';
-	generalControllerUrl = 'app/mvc/controller';
-	generalModelUrl = 'app/mvc/model';
-	generalViewUrl = 'app/mvc/view';
-
-=======
+	generalIP = 'http://192.168.0.15';
 	//generalIP = 'http://localhost';
 	generalUrl 				= generalIP +'/gestorFoodMobile/';
 	generalControllerUrl 	= 'app/mvc/controller';
 	generalModelUrl 		= 'app/mvc/model';
 	generalViewUrl 			= 'app/mvc/view';
 	
->>>>>>> parent of 45c4669... Corrigido bug que nao permitia enviar produtos para comandas
 	global_debug = 'no';
---
+++ readme.txt
@@ -0,0 +1,3 @@
+Autor : Michel Henrique Da Silva
+Empresa: Jointech Tecnologia em Sistemas
+Ultima Atualização:16/04/2014
