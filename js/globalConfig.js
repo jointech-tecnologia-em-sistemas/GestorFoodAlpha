5f80d5fbd10a412562e845c40a3efee888b9ea37
Witerlland Silva <witerlland@outlook.com>
2016-11-28 09:28:22 -0300

    Mudança no ip

--- js/globalConfig.js
+++ js/globalConfig.js
@@ -1,8 +1,10 @@
-	generalIP = 'http://192.168.0.15';
-	//generalIP = 'http://localhost';
+	generalIP = 'http://192.168.1.99';
+	// generalIP = 'http://localhost';
 	generalUrl 				= generalIP +'/gestorFoodMobile/';
 	generalControllerUrl 	= 'app/mvc/controller';
 	generalModelUrl 		= 'app/mvc/model';
 	generalViewUrl 			= 'app/mvc/view';
 	
+	transferencia = true;
+
 	global_debug = 'no';
